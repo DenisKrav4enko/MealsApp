d077965ff8be1a64a82d444be39bfb4f10881904
Denis <denys@techtio.io>
2025-02-01 14:03:38 +0200

    added meal detail screen

---
+++ screens/MealDetailScreen.js
@@ -0,0 +1,11 @@
+import {Text} from "react-native";
+
+function MealDetailScreen({ route }) {
+    const mealId = route.params.mealId
+
+    return(
+        <Text>MealDetailScreen ({mealId})</Text>
+    )
+}
+
+export default MealDetailScreen
