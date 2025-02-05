a49345c7491b5b116db01030d51672e69a2f07ca
Denis <denys@techtio.io>
2025-02-05 14:55:56 +0200

    use redux-toolkit

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,14 +1,16 @@
-import { useContext } from 'react'
+// import { useContext } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
-import { FavoritesContext } from '../store/context/favorites-context'
+import { useSelector } from 'react-redux'
+// import { FavoritesContext } from '../store/context/favorites-context'
 import { MEALS } from '../data/dummy-data'
 import MealsList from '../components/MealsList/MealsList'
 
 function FavoritesScreen() {
-    const favoriteMealsCtx = useContext(FavoritesContext);
+    // const favoriteMealsCtx = useContext(FavoritesContext);
+    const favoriteMealsIds = useSelector(state => state.favoriteMeals.ids)
 
     const favoriteMeals = MEALS.filter(meal =>
-        favoriteMealsCtx.ids.includes(meal.id)
+        favoriteMealsIds.includes(meal.id)
     )
 
     if (favoriteMeals.length === 0) {
